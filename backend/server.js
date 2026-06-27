3756c37e71ea94262040a9e6346a20a578fd6f51
lavetimohankarthik-svg <lavetimohankarthik@gmail.com>
2026-06-27 12:47:48 +0530

    Use Gmail SMTP host

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ console.log("Backend starting...");
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
